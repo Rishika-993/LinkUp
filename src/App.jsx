dcbe982b8794b3a4fa368ea94ce3bb1f02171c21
Rishika-993 <rishikaagrawal22@gmail.com>
2025-08-10 23:57:42 +0530

    Initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+      <div className="app-container">
+        <main className="main-content">
+          Hello World
+        </main>
+      </div>
+  );
+}
+
+export default App;
